db3160fbd1d9e36d3eee94f23004ba3b2acbdcd9
MxWyX <Max.wykes@gmail.com>
2022-10-05 18:30:17 +0100

    Make dice game BETTER

--- JavaScript/Week 1/Day 3/app.js
+++ JavaScript/Week 1/Day 3/app.js
@@ -1,25 +1,26 @@
 
-let dice2 = Math.floor(Math.random()*6);
-let dice1 = Math.floor(Math.random()*6);
+function getDice() {
+  return Math.floor(Math.random()*6)+1;
+}
 
 function numCheck(number1,number2) {
-  if ((number1 + number2) == 12) {
+  console.log(number1);
+  console.log(number2);
+  if ((number1 + number2) === 12) {
     console.log('You\'ve won a cuddly bear!');
-    console.log(number1);
-    console.log(number2);
-  } else if (number1 == number2) {
+  } else if (number1 === number2) {
     console.log('You get 3 free throws!');
-    console.log(number1);
-    console.log(number2);
-  } else if ((number1 + number2) % 2 == 0) {
+  } else if ((number1 + number2) % 2 === 0) {
     console.log('You win 1 free throw!');
-    console.log(number1);
-    console.log(number2);
   } else {
     console.log('You lose!');
-    console.log(number1);
-    console.log(number2);
   }
 };
 
-numCheck(dice1,dice2);
+function game() {
+  let dice1 = getDice();
+  let dice2 = getDice();
+  numCheck(dice1,dice2);
+}
+
+game();
