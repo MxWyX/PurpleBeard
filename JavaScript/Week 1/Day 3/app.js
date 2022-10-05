66788a3777471df505b002d73188ba4b768abb8b
MxWyX <Max.wykes@gmail.com>
2022-10-05 19:13:23 +0100

    Added rock paper scissors game

--- JavaScript/Week 1/Day 3/app.js
+++ JavaScript/Week 1/Day 3/app.js
@@ -4,23 +4,83 @@ function getDice() {
 }
 
 function numCheck(number1,number2) {
-  console.log(number1);
-  console.log(number2);
+  
   if ((number1 + number2) === 12) {
-    console.log('You\'ve won a cuddly bear!');
+    alert('You\'ve won a cuddly bear!');
   } else if (number1 === number2) {
-    console.log('You get 3 free throws!');
+    alert('You get 3 free throws!');
   } else if ((number1 + number2) % 2 === 0) {
-    console.log('You win 1 free throw!');
+    alert('You win 1 free throw!');
   } else {
-    console.log('You lose!');
+    alert('You lose!');
   }
 };
 
+
+function getUserChoice() {
+  const userChoice = prompt('Rock, Paper, Scissors?')
+  if (userChoice.toLowerCase() === 'rock' || userChoice.toLowerCase() === 'paper' || userChoice.toLowerCase() === 'scissors') {
+    return userChoice;
+  } else {
+    return 'Error';
+  } /* Making the input lower case if it is acceptable, otherwise throw an error. */
+}
+
+function getComputerChoice() {
+  const number = Math.floor(Math.random() * 3); /* random float between 0-1, times 3 and rounded gives a random whoe number of 0,1 or 2.*/
+  if (number === 0) {
+    return 'rock';
+  } else if (number === 1) {
+    return 'paper';
+  } else {
+    return 'scissors';
+  } /* Create a random number and use it to pick rock, paper or scissors. */
+}
+
+function determineWinner(userChoice,computerChoice) {
+  if (userChoice === computerChoice) { /* Check for a tie. */
+    return 'Tie.';
+  } else if (userChoice === 'rock') { /* Check if the computer beats the users choice. */
+    if (computerChoice === 'paper') { 
+      return 'Computer won.'; 
+    } else { /* If the player did make this choice and the computer didn't just win, the player must have won. */
+      return 'Player won.'; 
+    }
+  } else if (userChoice === 'paper') { /* Check if the computer beats the users choice again. */
+    if (computerChoice === 'scissors') {
+      return 'Computer won.';
+    } else { /* If the player did make this choice and the computer didn't just win, the player must have won. */
+      return 'Player won.';
+    }
+  } else if (userChoice === 'scissers') { /* Check if the computer beats the user for a final time. */
+    if (computerChoice === 'rock') {
+      return 'Computer won.';
+    } else { /* If the player did make this choice and the computer didn't just win, the player must have won. */
+      return 'Player won.';
+    }
+  } else {
+    return 'what...';
+  } /* If no one was won then things went very wrong some how... */
+}
+
 function game() {
-  let dice1 = getDice();
-  let dice2 = getDice();
-  numCheck(dice1,dice2);
+  const gameType = prompt('To play Dice enter: dice\nTo play Rock Paper Scissors enter: rps');
+  if (gameType.toLowerCase() === ('dice')) {
+    const dice1 = getDice();
+    const dice2 = getDice();
+    numCheck(dice1,dice2);
+  } else if (gameType.toLowerCase() === ('rps')) {
+    const userInput = getUserChoice(); /* Clean userChoice from the HTML file. */
+    if (userInput === 'Error') {
+      alert(input+' is not a valid input.'); /* If input is erroneous, break. */
+    } else {
+      const computerChoice = getComputerChoice(); /* Get a random computer choice. */
+      const winner = determineWinner(userInput,computerChoice); /* Check who won and set that to a variable. */
+      alert('The computer chose: '+computerChoice+'\nYou chose: '+userInput+'\n'+winner);  /* create a pop up in the window with who won.*/
+    }
+  } else {
+    alert('Somethings gone wrong...')
+  }
 }
 
 game();
