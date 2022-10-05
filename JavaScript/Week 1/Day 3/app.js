05f336a58f6d8b7d81fa7941a6c4deabb1cc839e
MxWyX <Max.wykes@gmail.com>
2022-10-05 18:07:39 +0100

    add dice game

--- JavaScript/Week 1/Day 3/app.js
+++ JavaScript/Week 1/Day 3/app.js
@@ -5,7 +5,7 @@ function numCheck(number1,number2) {
     console.log('You\'ve won a cuddly bear!');
   } else if (number1 == number2) {
     console.log('You get 3 free throws!')
-  } else if ((number1 % 2 == 0 ) || (number2 % 2 == 0)) {
+  } else if ((number1 + number2) % 2 == 0) {
     console.log('You win 1 free throw!')
   } else {
     console.log('You lose!')
@@ -20,6 +20,8 @@ function diceGet() {
 function diceGame() {
   const dice1 = diceGet
   const dice2 = diceGet
+  console.log(dice1)
+  console.log(dice2)
   numCheck(dice1,dice2)
 }
 
