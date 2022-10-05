01344e6842bb1f17fe67d4db3ed36a3a0b341607
MxWyX <Max.wykes@gmail.com>
2022-10-05 18:19:05 +0100

    Fix dice game

--- JavaScript/Week 1/Day 3/app.js
+++ JavaScript/Week 1/Day 3/app.js
@@ -1,28 +1,25 @@
 
+let dice2 = Math.floor(Math.random()*6);
+let dice1 = Math.floor(Math.random()*6);
 
 function numCheck(number1,number2) {
-  if ((number1 == 6) && (number2 == 6)) {
+  if ((number1 + number2) == 12) {
     console.log('You\'ve won a cuddly bear!');
+    console.log(number1);
+    console.log(number2);
   } else if (number1 == number2) {
-    console.log('You get 3 free throws!')
+    console.log('You get 3 free throws!');
+    console.log(number1);
+    console.log(number2);
   } else if ((number1 + number2) % 2 == 0) {
-    console.log('You win 1 free throw!')
+    console.log('You win 1 free throw!');
+    console.log(number1);
+    console.log(number2);
   } else {
-    console.log('You lose!')
+    console.log('You lose!');
+    console.log(number1);
+    console.log(number2);
   }
 };
 
-
-function diceGet() {
-  return (Math.floor(Math.random()*6))
-}
-
-function diceGame() {
-  const dice1 = diceGet
-  const dice2 = diceGet
-  console.log(dice1)
-  console.log(dice2)
-  numCheck(dice1,dice2)
-}
-
-diceGame()
+numCheck(dice1,dice2);
