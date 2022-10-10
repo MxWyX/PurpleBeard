5844f630c9786ee74e7dc3bf6f4a0e260ed1f035
MxWyX <Max.wykes@gmail.com>
2022-10-10 19:31:42 +0100

    Complete array and list tasks

---
+++ JavaScript/Week 2/Day 1/app.js
@@ -0,0 +1,104 @@
+
+// // Arrays
+
+// Task 1
+// const brekkie = ['croissant', 'toast', 'coffee'];
+// console.log(brekkie);
+
+// brekkie.push('multivitamin');
+// console.log(brekkie);
+
+// brekkie.unshift('water');
+// console.log(brekkie);
+
+// console.log(brekkie.length);
+
+
+// Task 2
+// function lister(arr) {
+//   console.log(brekkie[-1]);
+//   console.log(arr[-1]);
+// };
+
+// lister(brekkie);
+
+// Task 3
+// const musicGenres = [
+//   'Rock',
+//   'Rap'
+// ];
+// console.log(musicGenres);
+
+// musicGenres.push('Jazz');
+// console.log(musicGenres);
+
+// musicGenres[Math.floor(musicGenres.length/2)] = 'Classical';
+// console.log(musicGenres);
+
+// musicGenres.unshift('Blues');
+// musicGenres.unshift('Reggae');
+// console.log(musicGenres);
+
+// Task 4
+
+// const sorter = (arr) => {
+//   return arr.sort();
+// };
+
+// console.log(sorter(brekkie));
+
+
+// Lists
+
+// Task 1
+// for (let i = 1; i < 11; i++) {
+//   console.log(i);
+// };
+
+// for (let i = 10; i > 0; i--) {
+//   console.log(i);
+// };
+
+// Task 2
+// const tasks = ['washing up', 'mopping', 'dusting'];
+
+// for (let i = 0; i < tasks.length; i++) {
+//   console.log(`Task: ${i+1}: ${tasks[i]}`);
+// }
+
+// Task 3
+
+// let i = 3;
+
+// while (i) {
+//   alert(i--);
+// }
+
+// Task 4
+
+// for (let i = 2; i <= 10; i+=2) {
+//   console.log(i);
+// }
+
+
+// Task 5
+
+// let x = 0
+// while (x < 3) {
+//   alert(`number ${x}`);
+//   x++;
+// }
+
+// Task 61
+
+// const numbers = [1,2,3,4,5,6];
+
+// function sumInput(arr) {
+//   let sum = 0;
+//   for (let i = 0; i<arr.length; i++) {
+//     sum+=arr[i];
+//   }
+//   return sum;
+// }
+
+// console.log(sumInput(numbers));
