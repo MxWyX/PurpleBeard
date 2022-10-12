59c81185a2ea6cd73963b1c05f597e6dfa256b97
MxWyX <Max.wykes@gmail.com>
2022-10-12 12:42:00 +0100

    Finish Exercise 1

--- JavaScript/Week 2/Day 3/app.js
+++ JavaScript/Week 2/Day 3/app.js
@@ -1,14 +1,16 @@
 // Task 1
 
-const spacecraft = {
-  size: "300m",
-  range: "500,000km",
-  model: "Nostromo",
-  capacity: "50 people",
-};
+// const spacecraft = {
+//   size: "300m",
+//   range: "5,000,000,000km",
+//   model: "Nostromo",
+//   capacity: "50 people",
+// };
 
-console.log(spacecraft.range);
+// console.log(spacecraft.range);
 
-for (let [key, value] of Object.entries(spacecraft)) {
-  console.log(`${key}: ${value}`);
-}
+// for (let [key, value] of Object.entries(spacecraft)) {
+//   console.log(`${key}: ${value}`);
+// }
+
+// Task 2
